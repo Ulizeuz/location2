e389b8c438b4557715d39d8602f89010f5deb0f7
Ulises Gomez Olguin <ulizeuz@gmail.com>
2020-08-21 15:23:20 -0500

    Initial Commit

---
+++ location2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  location2
+//
+//  Created by Ulises Gomez Olguin on 21/08/20.
+//  Copyright © 2020 Ulises Gomez Olguin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
